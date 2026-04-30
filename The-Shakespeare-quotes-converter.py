9236e8764c07c3fb65fc07b5afca999aa05cda66
faris-khayyat <faris.khayyat553@education.nsw.gov.au>
2026-04-30 22:48:23 +0000

    i changed the csv reader for the mai n code 'y'

--- The-Shakespeare-quotes-converter.py
+++ The-Shakespeare-quotes-converter.py
@@ -1,18 +1,19 @@
 import csv 
 def Shakespeare_translator(text):
  modern_map = {}
- with open('words.csv', mode='r') as file:
+ with open('dictionary.csv', mode='r') as file:
     reader = csv.reader(file)
     next(reader)  # Skips the header row (e.g., "old, modern")
     for row in reader:
-        # row[0] is the old word, row[1] is the modern word
-         modern_map[row[0].strip().lower()] = row[1].strip()
+                # row[0] is the old word, row[1] is the modern word
+                if len(row) >= 2:
+                         modern_map[row[0].strip().lower()] = row[1].strip()
 
 
     words = text.lower().split()
     translated  = [modern_map.get(word, word)for word in words]
     return " ".join(translated).capitalize()
-
+ 
 while True:
     user_input = input("Enter a Shakespearean Line (or 'Q' for quit): ")
     if user_input == 'Q' or user_input == 'q':
